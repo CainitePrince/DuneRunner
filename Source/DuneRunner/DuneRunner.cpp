15cab3de84110c0a1b0cbea1cf04389762bf76e0
Floris Groen <floris_dephion_tech@outlook.com>
2024-02-23 09:09:30 +0100

    Add VFX packs. Add components.

---
+++ Source/DuneRunner/DuneRunner.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "DuneRunner.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DuneRunner, "DuneRunner" );
